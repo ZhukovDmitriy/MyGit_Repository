acdef385691221c65e3cee178a51fb278a7acbcd
ZhukovDmitriy <Zhukov160288@gmail.com>
2022-03-10 15:59:44 +0200

    added RabbitMQ test

---
+++ Projects_Practice/RabbitMQ/RabbitMQ/Publisher/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+using System.Threading;
+
+namespace Publisher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var counter = 0;
+
+            do
+            {
+                int timeToSleep = new Random().Next(1000, 3000);
+                Thread.Sleep(timeToSleep);
+
+                var factory = new ConnectionFactory() { HostName = "localhost" };
+
+                using (var connection = factory.CreateConnection())
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "dev-queue",
+                        durable: true,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+                    string message = $"Message from publisher N {counter}";
+
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                        routingKey: "dev-queue",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine($"Message is sent into Default Exchangr [N:{counter++}]");
+                }
+            } while (true);
+        }
+    }
+}
