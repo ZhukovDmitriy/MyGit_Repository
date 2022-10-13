c03cc11c29ea7a491cca73f7cf99d0164c3c2481
ZhukovDmitriy <Zhukov160288@gmail.com>
2022-10-14 01:33:24 +0300

    simple_example_monads

---
+++ Projects_Practice/Monads/Monads/Monads/Maybe.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace Monads
+{
+    public class Maybe<T>
+    {
+        public T _val { get; set; }
+        public bool _isInitialized { get; set; }
+
+        private Maybe(T val, bool isInitialized) =>
+            (_val, _isInitialized) = (val, isInitialized);
+
+        public static Maybe<T> None() =>
+            new Maybe<T>(default, false);
+
+        public static Maybe<T> Just([NotNull] T val) =>
+             new Maybe<T>(val ?? throw new ArgumentNullException(nameof(val)), true);
+
+        public Maybe<TRes> Select<TRes>(Func<T, TRes> mapper) =>
+            _isInitialized ? Maybe<TRes>.Just(mapper(_val)) : Maybe<TRes>.None();
+
+        public TRes Match<TRes>(Func<T, TRes> onSome, Func<TRes> onEmpty) =>
+            _isInitialized ? onSome(_val) : onEmpty(); 
+    }
+}
---
+++ Projects_Practice/Monads/Monads/Monads/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Monads
+{
+    class Program
+    {
+        static Maybe<int> GetIntPromise(bool arg) =>
+            arg ? Maybe<int>.Just(10) : Maybe<int>.None();
+
+        static void Main(string[] args)
+        {
+            //Maybe<int> intPromise = GetIntPromise(true);
+            //Console.WriteLine($"{intPromise._val}, {intPromise._isInitialized}");
+
+            //Maybe<string> stringPromise = intPromise.Select(i =>
+            //{
+            //    Console.WriteLine($"int was initialized by value: {i}");
+            //    return i.ToString();
+            //});
+            //Console.WriteLine($"{stringPromise._val + 10}, {stringPromise._val.GetType()}");
+
+            //string message = stringPromise.Match<string>(s => s, () =>
+            //"There is no value in promise");
+            //Console.WriteLine(message);
+
+            // Основная форма
+            Console.WriteLine(GetIntPromise(true).Select(i =>
+            {
+                Console.WriteLine($"int was initialized by value: {i}");
+                return i.ToString();
+            })
+                .Match(s => s, () =>
+            "There is no value in promise"));
+        }
+    }
+}
